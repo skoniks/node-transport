ab7e8ce8a58772ff9a1e82cba2f7267e872a76fa
skoniks <skolya16@ya.ru>
2023-10-25 11:55:26 +0300

    IPC default path update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,10 +5,10 @@ import { Payload } from './types';
 
 export const uuid = () => crypto.randomUUID();
 export const ppid = () => (cluster.isPrimary ? process.pid : process.ppid);
-export const ppath = (name = 'ipc') =>
+export const ppath = (name = `${ppid()}`) =>
   process.platform === 'win32'
-    ? path.join('\\\\?\\pipe', `${ppid()}-${name}`)
-    : path.join('/tmp', `${ppid()}-${name}.sock`);
+    ? path.join('\\\\?\\pipe', `${name}`)
+    : path.join('/tmp', `${name}.sock`);
 export const payload = {
   encode: (data: Payload) => Buffer.from(JSON.stringify(data)),
   decode: (data: Buffer) => JSON.parse(data.toString()) as Payload,
